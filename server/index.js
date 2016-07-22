43dbbced106e962265c4fe166e4d07ed741dd319
Andrew Bresee <abresee@PA-ABRESEE-M.local>
2016-07-22 14:58:31 -0700

    Begin implementation of mysql connection

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+// index.js
+
 /* eslint-disable no-console, no-shadow */
 import path from 'path';
 import webpack from 'webpack';
@@ -11,7 +13,33 @@ import config from './config/environment';
 import schema from './data/schema';
 import passport from 'passport';
 // will be useful after database is setup
-import User from './data/database'
+// import User from './data/database'
+
+import mysql from 'mysql';
+
+// connct to the db
+const connection = mysql.createConnection({
+  host: 'ip-10-2-1-250',
+  user: 'abresee',
+  password: 'SmartTest1234',
+  database: 'RelayFullstack'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log(chalk.red('There was an error connecting to the DB'));
+    return;
+  }
+  console.log(chalk.green('Connection to the DB has been made'));
+});
+
+// Passport for google oauth
+passport.serializeUser((accessToken, done) => {
+  // does not have the fields we created earlier, user.uid does not exist.
+  console.log(' === serialized === : ', accessToken);
+  // this sets req.session.passport.user = accessToken;
+  done(null, accessToken);
+});
 
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 // The UI needs to communicate with the server, then the server makes the calls
@@ -22,17 +50,11 @@ passport.use(new GoogleStrategy({
   callbackURL: 'http://localhost:3000/auth/google/callback'
 },
 
-  function(accessToken, refreshToken, profile, done) {
+  (accessToken, refreshToken, profile, done) => {
     console.log('accessToken: ', accessToken);
     console.log('refreshToken: ', refreshToken);
     console.log('profile: ', profile);
     console.log('done: ', done);
-
-    passport.serializeUser((accessToken, done) => {
-      // does not have the fields we created earlier, user.uid does not exist.
-      console.log('profile.id: ', accessToken);
-      done(null, accessToken);
-    });
 
     return done(null, accessToken);
 
@@ -59,6 +81,7 @@ function setupServerAuth(expressApp) {
     passport.authenticate('google', { failureRedirect: '/login' }),
     (req, res) => {
       console.log('== Google Response ==');
+      // Window.sessionStorage.setItem(req.session.passport.user);
       res.redirect('/');
     });
   return expressApp;
@@ -97,6 +120,7 @@ if (config.env === 'development') {
   // Setup auth *with* endpoints
   // relayServer.use('/googleAuth', googleAuth);
   relayServer.use(passport.initialize());
+  relayServer.use(passport.session());
   relayServer.use('/', setupServerAuth(express()));
 
    // Serve static resources
